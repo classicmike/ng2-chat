415a86e2c95f8067cf3064b523b3dd5c23804956
Michael Tran <koramaiku@Michaels-iMac.local>
2017-01-22 11:38:39 +1100

    Finished Project

---
+++ app/components/chat/chat.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnDestroy, OnInit} from "@angular/core";
+import {ChatService} from "../../services/chat.service";
+
+
+@Component({
+  moduleId: module.id,
+  selector: 'chat',
+  templateUrl: 'chat.component.html'
+})
+
+export class ChatComponent implements OnInit, OnDestroy {
+  messages: any = [];
+  message: string;
+  connection: any;
+  username: string;
+  alert: any = false;
+
+  constructor(private _chatService: ChatService) {
+
+  }
+
+  sendMessage() {
+    this._chatService.sendMessage(this.message, this.username);
+    this.message = '';
+  }
+
+  ngOnInit() {
+
+    this.connection = this._chatService.getMessages()
+      .subscribe(message => {
+        this.messages.push(message);
+      })
+  }
+
+  ngOnDestroy() {
+    this.connection.unsubscribe();
+  }
+
+  setUsername(){
+    this._chatService.setUsername(this.username);
+    this.alert = 'Username is set';
+  }
+}
